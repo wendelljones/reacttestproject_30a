17d05f81232707eef6bb445571f1851e343a1c4d
Wendell Jones <wendelljones@Wendells-MBP.lan>
2024-08-21 17:30:54 -0500

    map doesn't work yet

--- src/components/maps.js
+++ src/components/maps.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import React, { useState, useEffect } from "react";
+import { LoadScript, GoogleMap, InfoWindow } from "@react-google-maps/api";
 import '../styles/maps.css';
 
+/* global google */
+
 function GoogleMapFor30() {
+    const [map, setMap] = useState(null);
     const [selected, setSelected] = useState(null);
 
     const locations = [
@@ -13,7 +16,8 @@ function GoogleMapFor30() {
             description: "Famed 4,200 ft art deco suspension bridge open to car, bike & foot traffic.",
             image: "./images/goldenGate_300x169.jpg" 
         },
-        {   lat: 32.776665,  
+        {   
+            lat: 32.776665,  
             lng: -96.796989, 
             name: "Dallas Texas", 
             description: "Dallas, a modern metropolis in north Texas, is a commercial and cultural hub of the region. Downtown’s Sixth Floor Museum at Dealey Plaza commemorates the site of President John F. Kennedy’s assassination in 1963. In the Arts District, the Dallas Museum of Art and the Crow Collection of Asian Art cover thousands of years of art. The sleek Nasher Sculpture Center showcases contemporary sculpture.",
@@ -21,33 +25,44 @@ function GoogleMapFor30() {
         }
     ];
 
+    useEffect(() => {
+        if (map && google?.maps?.marker?.AdvancedMarkerElement) {
+            locations.forEach(location => {
+                const marker = new google.maps.marker.AdvancedMarkerElement({
+                    position: { lat: location.lat, lng: location.lng },
+                    map: map,
+                    title: location.name
+                });
+
+                marker.addListener('click', () => {
+                    setSelected(location);
+                });
+            });
+        }
+    }, [map]);
+
     return (
         <div className="mapDiv">
-        <LoadScript googleMapsApiKey="AIzaSyA_V4J1t09TrGyClkuYzvENZvJoba15i2c">
-            <GoogleMap 
-                mapContainerStyle={{ width: '100%', height: '450px', margin: '0px', border: '2px solid #500000', borderRadius: '15px' }}
-                center={{ lat: 35.519119, lng: -108.739769 }}
-                zoom={5}
+            <LoadScript
+                googleMapsApiKey="AIzaSyCw-T8ae_M-ieKNzowz2XMm_Rc-rB3vRSM"
+                libraries={['marker']}  // Static libraries array
+            >
+                <GoogleMap 
+                    mapId="17c5b936d351f783"
+                    mapContainerStyle={{ width: '100%', height: '450px', margin: '0px', border: '2px solid #500000', borderRadius: '15px' }}
+                    center={{ lat: 35.519119, lng: -108.739769 }}
+                    zoom={5}
+                    onLoad={map => setMap(map)}
                 >
-                    {locations.map((location, i) => (
-                        <Marker
-                            key={i}
-                            position={{ lat: location.lat, lng: location.lng }}
-                            onClick={() => setSelected(location)}
-                        />
-                    ))}
-
                     {selected && (
                         <InfoWindow
                             position={{ lat: selected.lat, lng: selected.lng }}
                             onCloseClick={() => setSelected(null)}
                         >
                             <div className="locationCallout">
-                                <button className="buttonStyle"
-                                    onClick={() => setSelected(null)}
-                                    >
-                                        X
-                                    </button>
+                                <button className="buttonStyle" onClick={() => setSelected(null)}>
+                                    X
+                                </button>
                                 <img className="calloutImg" src={selected.image} alt={selected.name} />
                                 <h4>{selected.name}</h4>
                                 <p>{selected.description}</p>
@@ -55,7 +70,7 @@ function GoogleMapFor30() {
                         </InfoWindow>
                     )}
                 </GoogleMap>
-        </LoadScript>
+            </LoadScript>
         </div>
     )
 }
